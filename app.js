3210bbf2ba8724335f2372d222fdb1e7be54e34a
akzReekoh <achilles.dinglasan@p3ople4u.com>
2016-03-08 17:51:40 +0800

    REEKOH-88
    -Updated travis.yml
    -Updated app.js to accept array input
    -Bumped version
    -Updated test suite to test array input

--- app.js
+++ app.js
@@ -2,36 +2,47 @@
 
 var platform = require('./platform'),
     isPlainObject = require('lodash.isplainobject'),
+    isArray = require('lodash.isarray'),
+    async = require('async'),
 	sqs, queueUrl;
+
+let sendData = (data) => {
+    var params = {
+        MessageBody : JSON.stringify(data),
+        QueueUrl : queueUrl,
+        MessageAttributes :{
+            Attributes:{
+                DataType : 'String',
+                StringValue : JSON.stringify(data)
+            }
+        }
+    };
+
+    sqs.sendMessage(params, function(error, response){
+        if(error){
+            console.error(error);
+            platform.handleException(error);
+        }
+        else{
+            platform.log(JSON.stringify({
+                title: 'AWS SQS message saved.',
+                data: data
+            }));
+        }
+    });
+};
 
 platform.on('data', function (data) {
 	if(isPlainObject(data)){
-        var params = {
-            MessageBody : JSON.stringify(data),
-            QueueUrl : queueUrl,
-            MessageAttributes :{
-                Attributes:{
-                    DataType : 'String',
-                    StringValue : JSON.stringify(data)
-                }
-            }
-        };
-
-        sqs.sendMessage(params, function(error, response){
-            if(error){
-                console.error(error);
-                platform.handleException(error);
-            }
-            else{
-                platform.log(JSON.stringify({
-                    title: 'AWS SQS message saved.',
-                    data: data
-                }));
-            }
+        sendData(data);
+    }
+    else if(isArray(data)){
+        async.each(data, (datum) => {
+            sendData(datum);
         });
     }
     else
-        platform.handleException(new Error('Invalid data received. Must be a valid JSON Object. Data ' + data));
+        platform.handleException(new Error('Invalid data received. Must be a valid Array/JSON Object. Data ' + data));
 });
 
 platform.once('close', function () {
